60703106f0f517812f6dd222f95e67a0dcec7bd6
Rafael <rafa.dev.oliveira@gmail.com>
2023-10-20 22:22:38 +0000

    Updated

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-async function main () {
-    const Produto = mongoose.model('Produto',
-    {
-        codigo: Number,
-        nome: String,
-        preco: Number
-    })
+const Produto = mongoose.model('Produto',
+{
+    codigo: Number,
+    nome: String,
+    preco: Number
+})
 
-    Produto.create({
-        codigo: 1,
-        nome: "Monitor AOC FullHD 144hz",
-        preco: 1100
-    })
-
-    await Produto.findOneAndUpdate(
-        {preco: 1100},
-        {
-            codigo: 2,
-            nome: "Monitor Genérico DELL",
-            preco: 400
-        }
-    )
-
-    await Produto.findOneAndDelete({preco: 400})
-
-    
-    console.log(await Produto.find())
-
-    console.log(await Produto.findOne({preco: 1100}))
-
+async function criaNovoProduto(produto) {
+    await Produto.create(produto)
 }
 
-main()
+async function atualizaProduto(nomeAProcurar, novoCodigo, novoNome, novoPreco) {
+    await Produto.findOneAndUpdate(
+        {nome: nomeAProcurar},
+        {
+            codigo: novoCodigo,
+            nome: novoNome,
+            preco: novoPreco
+        }
+    )
+}
+
+async function deletaProduto(nomeADeletar){
+    await Produto.findOneAndDelete({nome: nomeADeletar})
+}
+
+async function exibeTodosOsProdutos(){
+    console.log(await Produto.find())
+}
+
+async function exibeUmProduto (nomeAProcurar) {
+    console.log(await Produto.findOne({nome: nomeAProcurar}))
+}
+
+criaNovoProduto(129192, "Monitor", 1000)
+atualizaProduto(129192, 1231, "TV", 1300)
+deletaProduto(129192)
+exibeTodosOsProdutos()
+exibeUmProduto(129192)
+
+const produtos = [
+    {nome: "monitor", codigo: 1001, preco: 300},
+    {nome: "lorem", codigo: 12101, preco: 301320},
+    {nome: "dor", codigo: 1011201, preco: 30110}
+]
+condicao = true
+
+while(condicao){
+    criaNovoProduto()
+}
+
+criaNovoProduto(produtos[0])
